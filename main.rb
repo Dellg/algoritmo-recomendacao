af11329d957eb632cff8154d5738bbbeab03b18c
Wendell Gomes <wendell_rpg@hotmail.com>
2017-12-11 23:07:10 -0300

    Adicionado novas iterações para os outros arquivos.

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ class Main
   colab = Colaborativo.new
 
   # inicialização dos usuários e filmes com rating inicial = 0
-  puts "Criando usuários..."
+  puts "\nCriando usuários...\n\n"
   usuarios = []
   dados = []
   for i in 1..943
@@ -15,30 +15,36 @@ class Main
     end
   end
 
-  # lendo dados do arquivo base e associando o rating de cada filme a cada usuário
-  puts "Lendo arquivo dos ratings e associando a cada usuário..."
-  arquivo = File.open("./dados/u1.base", "r")
-  arquivo.each_line do |linha|
-    linha = linha.split("\t")
-    usuarios[linha[0].to_i][linha[1].to_i] = linha[2].to_i
+  for iteracao in 1..5
+    puts "\tIteração #{iteracao}:"
+
+    # lendo dados do arquivo base e associando o rating de cada filme a cada usuário
+    puts "Lendo arquivo u#{iteracao}.base com os ratings e associando a cada usuário..."
+    arquivo = File.open("./dados/u#{iteracao}.base", "r")
+    arquivo.each_line do |linha|
+      linha = linha.split("\t")
+      usuarios[linha[0].to_i][linha[1].to_i] = linha[2].to_i
+    end
+
+    puts "Calculando os 10 vizinhos mais próximos de cada usuário..."
+    vizinhos = colab.similaridadeCossenos(usuarios)
+
+    puts "Calculando os ratings previstos para cada usuário com base nos ratings dos vizinhos..."
+    previsao = colab.calcularPrevisao(usuarios, vizinhos)
+
+    # lendo dados do arquivo de teste para ser possível comparar com os dados base
+    puts "Lendo arquivo u#{iteracao}.test com os ratings de teste para comparar..."
+    arquivo2 = File.open("./dados/u#{iteracao}.test", "r")
+    arquivo2.each_line do |linha|
+      linha = linha.split("\t")
+      dados[linha[0].to_i][linha[1].to_i] = linha[2].to_i
+    end
+
+    # comparando dados base com teste
+    puts "Comparando dados base com dados teste..."
+    rmse = colab.rootMeanSquareError(previsao, dados)
+    puts "Root Mean Square Error = #{rmse}\nFinalizando Iteração #{iteracao}\n\n"
   end
-
-  puts "Calculando os 10 vizinhos mais próximos de cada usuário..."
-  vizinhos = colab.similaridadeCossenos(usuarios)
-
-  puts "Calculando os ratings previstos para cada usuário com base nos ratings dos vizinhos..."
-  previsao = colab.calcularPrevisao(usuarios, vizinhos)
-
-  # lendo dados do arquivo de teste para ser possível comparar com os dados base
-  puts "Lendo arquivo dos ratings de teste para comparar..."
-  arquivo2 = File.open("./dados/u1.test", "r")
-  arquivo2.each_line do |linha|
-    linha = linha.split("\t")
-    dados[linha[0].to_i][linha[1].to_i] = linha[2].to_i
-  end
-
-  # comparando dados base com teste
-  puts "Comparando dados base com dados teste..."
-  rmse = colab.rootMeanSquareError(previsao, dados)
-  puts "Root Mean Square Error = #{rmse}"
+  puts "Digite algo para finalizar..."
+  gets
 end
